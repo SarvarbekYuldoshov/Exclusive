4f120e002d54231a45e5f321e93623d89cc0a69e
Sarvarbek Yuldoshov <sarvarbekyuldoshov85@gmail.com>
2024-08-29 09:22:26 +0300

    salom

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import "./Header.css"
 import Rasm_1 from "../images/rasm1.png"
 import Rasm_2 from "../images/rasm2.png"
+import Rasm_3 from "../images/rasm3.png"
+import Rasm_4 from "../images/rasm4.png"
 const Header = () => {
   return (
     <div>
@@ -13,11 +15,13 @@ const Header = () => {
             <li><a className='text-[16px] text-[#000000] text-center' href="#">About</a></li>
             <li><a className='text-[16px] text-[#000000] text-center' href="#">Sign in</a></li>
         </ul>
-        <ul className='flex'>
-            <button>
-                <input type="text" placeholder='What are you looking for?' />
-                <img src={Rasm_2} alt="" />
-            </button>
+        <ul className='flex gap-5'>
+            <div className='flex gap-20 border border-black'>
+                <input type="text" placeholder='What are you looking for?' className='hover:border-white' />
+                <button className=''><img src={Rasm_2} alt="" /></button>
+            </div>
+            <img src={Rasm_3} alt="" />
+            <img src={Rasm_4} alt="" />
         </ul>
       </div>
     </div>
