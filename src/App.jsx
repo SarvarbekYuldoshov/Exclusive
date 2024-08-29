0614b96cbf2130502ae90b2cddf5a424af970f46
Sarvarbek Yuldoshov <sarvarbekyuldoshov85@gmail.com>
2024-08-29 07:31:19 +0300

    salom

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import './App.css'
+import Header from './Components/Header/Header'
+import './index.css'
 
 function App() {
 
   return (
     <>
-     <div className='App'>
-
+     <div className='App'> 
+        <Header/>
      </div>
     </>
   )
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Header.css"
+import Rasm_1 from "../images/rasm1.png"
+const Header = () => {
+  return (
+    <div>
+      <div className='max-w-screen-lg'>
+           <img src={Rasm_1} alt="" />
+        <ul className=''></ul>
+      </div>
+    </div>
+  )
+}
+
+export default Header
