b562cc1ddf9af4a744d2e7c7025014a8b4b665d5
Sarvarbek Yuldoshov <sarvarbekyuldoshov85@gmail.com>
2024-09-02 13:36:48 +0300

    navbar qimini hato va kamchiliklarini tugirladim va ohirigacha tugatdim

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,30 +1,181 @@
-import React from 'react'
-import Rasm_8 from "../images/rasm8.png"
-import Rasm_16 from "../images/rasm16.png"
-import Rasm_17 from "../images/rasm17.png"
-import Rasm_18 from "../images/rasm18.png"
+import React from 'react';
+import Rasm_8 from "../images/rasm8.png";
+import Rasm_16 from "../images/rasm16.png";
+import Rasm_17 from "../images/rasm17.png";
+import Rasm_18 from "../images/rasm18.png";
+import Rasm_19 from "../images/rasm19.png";
+import Rasm_20 from "../images/rasm20.png"
+import Rasm_21 from "../images/rasm21.png"
+import Rasm_22 from "../images/rasm22.png"
+import Rasm_23 from "../images/rasm23.png"
+import Rasm_24 from "../images/rasm24.png"
+import Rasm_25 from "../images/rasm25.png"
+
 const Navbar = () => {
   return (
     <div className=''>
       <div className='mx-auto max-w-[1200px] p-[20px]'>
-           <ul className='flex items-center gap-10 mt-[40px]'>
-              <img className='' src={Rasm_8} alt="" />
-                <p className='font-poppins text-[16px] font-semibold leading-[20px] text-left text-[#DB4444] w-[150px] h-[20px]'>Our Products</p>
-           </ul>
-           <div className=''>
-            <ul className=''>
-              <li className='mt-20 bg-[#F5F5F5] justify-center w-[270px] h-[250px]'>
-                <img className='w-[115px] h-[180px] mt-10' src={Rasm_16} alt=" " />
-              </li>
-              <li>
-                <img className='w-[24px] h-[24px]' src={Rasm_17} alt="" />
-                <img className='w-[34px] h-[34px]' src={Rasm_18} alt="" />
-              </li>
-            </ul>
-           </div>
+        <ul className='flex items-center gap-10 mt-[40px]'>
+          <img className='' src={Rasm_8} alt="" />
+          <p className='font-poppins text-[16px] font-semibold leading-[20px] text-left text-[#DB4444] w-[150px] h-[20px]'>Our Products</p>
+        </ul>
+        <div className='flex flex-wrap gap-[20px]'>
+        <div className='mt-20'>
+          <div className='relative flex flex-col items-center bg-[#F5F5F5] w-[270px] h-[250px] p-4'>
+            <img className='w-[115px] h-[180px] mt-[20px]' src={Rasm_16} alt=" " />
+            <div className='absolute top-4 right-4 flex flex-col items-end space-y-2'>
+              <img className='w-[30px] h-[30px]' src={Rasm_17} alt="" />
+              <img className='w-[34px] h-[34px]' src={Rasm_18} alt="" />
+            </div>
+          </div>
+          <p className='mt-5 w-[208px] h-[24px] font-poppins text-[16px] font-medium leading-[24px] text-left bg-black-custom'>
+            Breed Dry Dog Food
+          </p>
+          <ul className='flex gap-2 items-center'>
+            <p>$100</p>
+            <img src={Rasm_19} alt="" />
+            <p className='w-[32px] h-[20px] font-poppins text-[14px] font-semibold leading-[21px] text-left bg-black-custom'>
+              (35)
+            </p>
+          </ul>
+        </div>
+        <div className='mt-20'>
+          <div className='relative flex flex-col items-center bg-[#F5F5F5] w-[270px] h-[250px] p-4'>
+            <img className='w-[146px] h-[163px] mt-[20px]' src={Rasm_20} alt=" " />
+            <div className='absolute top-4 right-4 flex flex-col items-end space-y-2'>
+              <img className='w-[30px] h-[30px]' src={Rasm_17} alt="" />
+              <img className='w-[34px] h-[34px]' src={Rasm_18} alt="" />
+            </div>
+          </div>
+          <p className='mt-5 w-[208px] h-[24px] font-poppins text-[16px] font-medium leading-[24px] text-left bg-black-custom'>
+          CANON EOS DSLR Camera
+          </p>
+          <ul className='flex gap-2 items-center'>
+            <p>$360</p>
+            <img src={Rasm_19} alt="" />
+            <p className='w-[32px] h-[20px] font-poppins text-[14px] font-semibold leading-[21px] text-left bg-black-custom'>
+              (95)
+            </p>
+          </ul>
+        </div>
+        <div className='mt-20'>
+          <div className='relative flex flex-col items-center bg-[#F5F5F5] w-[270px] h-[250px] p-4'>
+            <img className='w-[190px] h-[180px] mt-[20px]' src={Rasm_21} alt=" " />
+            <div className='absolute top-4 right-4 flex flex-col items-end space-y-2'>
+              <img className='w-[30px] h-[30px]' src={Rasm_17} alt="" />
+              <img className='w-[34px] h-[34px]' src={Rasm_18} alt="" />
+            </div>
+          </div>
+          <p className='mt-5 w-[208px] h-[24px] font-poppins text-[16px] font-medium leading-[24px] text-left bg-black-custom'>
+          ASUS FHD Gaming Laptop
+          </p>
+          <ul className='flex gap-2 items-center'>
+            <p>$700</p>
+            <img src={Rasm_19} alt="" />
+            <p className='w-[32px] h-[20px] font-poppins text-[14px] font-semibold leading-[21px] text-left bg-black-custom'>
+              (325)
+            </p>
+          </ul>
+        </div>
+        <div className='mt-20'>
+          <div className='relative flex flex-col items-center bg-[#F5F5F5] w-[270px] h-[250px] p-4'>
+            <img className='w-[180px] h-[133px] mt-[40px] mr-[25px]' src={Rasm_22} alt=" " />
+            <div className='absolute top-4 right-4 flex flex-col items-end space-y-2'>
+              <img className='w-[30px] h-[30px]' src={Rasm_17} alt="" />
+              <img className='w-[34px] h-[34px]' src={Rasm_18} alt="" />
+            </div>
+          </div>
+          <p className='mt-5 w-[208px] h-[24px] font-poppins text-[16px] font-medium leading-[24px] text-left bg-black-custom'>
+          Kids Electric Car
+          </p>
+          <ul className='flex gap-2 items-center'>
+            <p>$960</p>
+            <img src={Rasm_19} alt="" />
+            <p className='w-[32px] h-[20px] font-poppins text-[14px] font-semibold leading-[21px] text-left bg-black-custom'>
+              (65)
+            </p>
+          </ul>
+        </div>
+        <div className='mt-20'>
+          <div className='relative flex flex-col items-center bg-[#F5F5F5] w-[270px] h-[250px] p-4'>
+            <img className='w-[186px] h-[164px] mt-[30px]' src={Rasm_23} alt=" " />
+            <div className='absolute top-4 right-4 flex flex-col items-end space-y-2'>
+              <img className='w-[30px] h-[30px]' src={Rasm_17} alt="" />
+              <img className='w-[34px] h-[34px]' src={Rasm_18} alt="" />
+            </div>
+          </div>
+          <p className='mt-5 w-[208px] h-[24px] font-poppins text-[16px] font-medium leading-[24px] text-left bg-black-custom'>
+          Jr. Zoom Soccer Cleats
+          </p>
+          <ul className='flex gap-2 items-center'>
+            <p>$1160</p>
+            <img src={Rasm_19} alt="" />
+            <p className='w-[32px] h-[20px] font-poppins text-[14px] font-semibold leading-[21px] text-left bg-black-custom'>
+              (35)
+            </p>
+          </ul>
+        </div>
+        <div className='mt-20'>
+          <div className='relative flex flex-col items-center bg-[#F5F5F5] w-[270px] h-[250px] p-4'>
+            <img className='w-[178px] h-[150px] mt-[20px]' src={Rasm_24} alt=" " />
+            <div className='absolute top-4 right-4 flex flex-col items-end space-y-2'>
+              <img className='w-[30px] h-[30px]' src={Rasm_17} alt="" />
+              <img className='w-[34px] h-[34px]' src={Rasm_18} alt="" />
+            </div>
+          </div>
+          <p className='mt-5 w-[208px] h-[24px] font-poppins text-[16px] font-medium leading-[24px] text-left bg-black-custom'>
+          GP11 Shooter USB Gamepad
+          </p>
+          <ul className='flex gap-2 items-center'>
+            <p>$660</p>
+            <img src={Rasm_19} alt="" />
+            <p className='w-[32px] h-[20px] font-poppins text-[14px] font-semibold leading-[21px] text-left bg-black-custom'>
+              (55)
+            </p>
+          </ul>
+        </div>
+        <div className='mt-20'>
+          <div className='relative flex flex-col items-center bg-[#F5F5F5] w-[270px] h-[250px] p-4'>
+            <img className='w-[183px] h-[176px] mt-[20px]' src={Rasm_25} alt=" " />
+            <div className='absolute top-4 right-4 flex flex-col items-end space-y-2'>
+              <img className='w-[30px] h-[30px]' src={Rasm_17} alt="" />
+              <img className='w-[34px] h-[34px]' src={Rasm_18} alt="" />
+            </div>
+          </div>
+          <p className='mt-5 w-[208px] h-[24px] font-poppins text-[16px] font-medium leading-[24px] text-left bg-black-custom'>
+          Quilted Satin Jacket
+          </p>
+          <ul className='flex gap-2 items-center'>
+            <p>$660</p>
+            <img src={Rasm_19} alt="" />
+            <p className='w-[32px] h-[20px] font-poppins text-[14px] font-semibold leading-[21px] text-left bg-black-custom'>
+              (55)
+            </p>
+          </ul>
+        </div>
+        <div className='mt-20'>
+          <div className='relative flex flex-col items-center bg-[#F5F5F5] w-[270px] h-[250px] p-4'>
+            <img className='w-[115px] h-[180px] mt-[20px]' src={Rasm_16} alt=" " />
+            <div className='absolute top-4 right-4 flex flex-col items-end space-y-2'>
+              <img className='w-[30px] h-[30px]' src={Rasm_17} alt="" />
+              <img className='w-[34px] h-[34px]' src={Rasm_18} alt="" />
+            </div>
+          </div>
+          <p className='mt-5 w-[208px] h-[24px] font-poppins text-[16px] font-medium leading-[24px] text-left bg-black-custom'>
+            Breed Dry Dog Food
+          </p>
+          <ul className='flex gap-2 items-center'>
+            <p>$100</p>
+            <img src={Rasm_19} alt="" />
+            <p className='w-[32px] h-[20px] font-poppins text-[14px] font-semibold leading-[21px] text-left bg-black-custom'>
+              (35)
+            </p>
+          </ul>
+          </div>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
