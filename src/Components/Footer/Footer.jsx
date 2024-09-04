905cfa22709c5bbbce2938ac3051597e77c3126b
Sarvarbek Yuldoshov <sarvarbekyuldoshov85@gmail.com>
2024-09-04 12:33:43 +0300

    salom

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,41 +1,165 @@
-import React from 'react'
-import Rasm_35 from "../images/rasm35.png"
+import React from 'react';
+import Rasm_35 from "../images/rasm35.png";
+import Rasm_36 from "../images/rasm36.png";
+import Rasm_37 from "../images/rasm37.png";
+import Rasm_38 from "../images/rasm38.png";
+import Rasm_39 from "../images/rasm39.png";
+import Rasm_40 from "../images/rasm40.png";
+import Rasm_41 from "../images/rasm41.png";
+import Rasm_42 from "../images/rasm42.png";
+
 const Footer = () => {
   return (
     <div className=''>
-      <div className='mx-auto mt-[50px] max-w-[1200px] p-[20px] bg-black relative flex'>
+      <div className='mx-auto mt-[50px] gap-[20px] max-w-[1200px] p-[20px] bg-black relative flex flex-wrap'>
         <div className=''>
-            <li className='mt-[20px]'><a className=' w-[118px] h-[24px] font-inter text-[24px] font-bold leading-[24px] tracking-[0.03em] text-left text-[#FAFAFA]' href="">Exclusive</a>
-            </li>
-            <li className='mt-[20px]'><a className='w-[101px] h-[28px] font-poppins text-[20px] font-medium leading-[28px] text-left text-[#FAFAFA]' href="">Subscribe</a>
-            </li>
-            <p className='mt-[10px] ml-[24px] w-[206px] h-[24px] font-poppins text-[16px] font-normal leading-[24px] text-left text-[#FAFAFA]'>Get 10% off your first order</p>
-            <input className='w-[217px] h-[48px] ml-[24px] mt-[20px] bg-transparent border border-white' type="text" placeholder='    Enter your email'/>
-            <img className='absolute left-[230px] bottom-[33px] text-[white]' src={Rasm_35} alt="" />
-        </div>
-        <div className=''>
+          <ul>
             <li className='mt-[20px]'>
-                <a className='w-[81px] h-[28px] font-poppins text-[20px] font-medium leading-[28px] text-left text-[#FAFAFA]' href="">Support</a>
+              <a className='w-[118px] h-[24px] font-inter text-[24px] font-bold leading-[24px] tracking-[0.03em] text-left text-[#FAFAFA]' href="">
+                Exclusive
+              </a>
             </li>
-               <p className='mt-[20px] ml-[24px] w-[175px] h-12 font-poppins text-[16px] font-normal leading-6 text-left text-[#FAFAFA]'>111 Bijoy sarani, Dhaka,  DH 1515, Bangladesh.</p>
+            <li className='mt-[20px]'>
+              <a className='w-[101px] h-[28px] font-poppins text-[20px] font-medium leading-[28px] text-left text-[#FAFAFA]' href="">
+                Subscribe
+              </a>
+            </li>
+            <p className='mt-[10px] ml-[24px] w-[206px] h-[24px] font-poppins text-[16px] font-normal leading-[24px] text-left text-[#FAFAFA]'>
+              Get 10% off your first order
+            </p>
+            <input className='w-[217px] h-[48px] ml-[24px] mt-[20px] bg-transparent border border-white' type="text" placeholder='Enter your email' />
+          </ul>
+        </div>
+
+        <div className=''>
+          <ul>
+            <li className='mt-[20px]'>
+              <a className='w-[81px] h-[28px] font-poppins text-[20px] font-medium leading-[28px] text-left text-[#FAFAFA]' href="">
+                Support
+              </a>
+            </li>
+            <p className='mt-[20px] ml-[24px] w-[175px] h-12 font-poppins text-[16px] font-normal leading-6 text-left text-[#FAFAFA]'>
+              111 Bijoy sarani, Dhaka, DH 1515, Bangladesh.
+            </p>
             <li className='mt-[10px] ml-[1px]'>
-               <a className='w-[100px] h-[30px] font-poppins text-[16px] font-normal leading-[24px] text-left text-[#FAFAFA]' href="">sarvarbekyuldoshov85@gmail.com</a>
+              <a className='w-[100px] h-[30px] font-poppins text-[16px] font-normal leading-[24px] text-left text-[#FAFAFA]' href="">
+                sarvarbekyuldoshov85@gmail.com
+              </a>
             </li>
-            <p className='mt-[10px] ml-[20px] w-[165px] h-[24px] font-poppins text-[16px] font-normal leading-[24px] text-left text-[#FAFAFA]'>+998 94 239 04 26</p>
+            <p className='mt-[10px] ml-[20px] w-[165px] h-[24px] font-poppins text-[16px] font-normal leading-[24px] text-left text-[#FAFAFA]'>
+              +998 94 239 04 26
+            </p>
+          </ul>
         </div>
+
         <div className=''>
+          <ul>
             <li className='mt-[20px]'>
-                <a className='w-[81px] h-[28px] font-poppins text-[20px] font-medium leading-[28px] text-left text-[#FAFAFA]' href="">Account</a>
+              <a className='w-[81px] h-[28px] font-poppins text-[20px] font-medium leading-[28px] text-left text-[#FAFAFA]' href="">
+                Account
+              </a>
             </li>
-            <li className='mt-[10px]'><a className='w-[94px] h-[24px] font-poppins text-[16px] font-normal leading-[24px] text-left text-[#FAFAFA]' href="">My Account</a></li>
-            <li className='mt-[10px]'><a className='w-[94px] h-[24px] font-poppins text-[16px] font-normal leading-[24px] text-left text-[#FAFAFA]' href="">Login / Register</a></li>
-            <li className='mt-[10px]'><a className='w-[94px] h-[24px] font-poppins text-[16px] font-normal leading-[24px] text-left text-[#FAFAFA]' href="">Cart</a></li>
-            <li className='mt-[10px]'><a className='w-[94px] h-[24px] font-poppins text-[16px] font-normal leading-[24px] text-left text-[#FAFAFA]' href="">Wishlist</a></li>
-            <li className='mt-[10px]'><a className='w-[94px] h-[24px] font-poppins text-[16px] font-normal leading-[24px] text-left text-[#FAFAFA]' href="">Shop</a></li>
+            <li className='mt-[10px]'>
+              <a className='w-[94px] h-[24px] font-poppins text-[16px] font-normal leading-[24px] text-left text-[#FAFAFA]' href="">
+                My Account
+              </a>
+            </li>
+            <li className='mt-[10px]'>
+              <a className='w-[94px] h-[24px] font-poppins text-[16px] font-normal leading-[24px] text-left text-[#FAFAFA]' href="">
+                Login / Register
+              </a>
+            </li>
+            <li className='mt-[10px]'>
+              <a className='w-[94px] h-[24px] font-poppins text-[16px] font-normal leading-[24px] text-left text-[#FAFAFA]' href="">
+                Cart
+              </a>
+            </li>
+            <li className='mt-[10px]'>
+              <a className='w-[94px] h-[24px] font-poppins text-[16px] font-normal leading-[24px] text-left text-[#FAFAFA]' href="">
+                Wishlist
+              </a>
+            </li>
+            <li className='mt-[10px]'>
+              <a className='w-[94px] h-[24px] font-poppins text-[16px] font-normal leading-[24px] text-left text-[#FAFAFA]' href="">
+                Shop
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div className=''>
+          <ul>
+            <li className='mt-[20px]'>
+              <a className='w-[81px] h-[28px] font-poppins text-[20px] font-medium leading-[28px] text-left text-[#FAFAFA]' href="">
+                Quick Link
+              </a>
+            </li>
+            <li className='mt-[10px]'>
+              <a className='w-[94px] h-[24px] font-poppins text-[16px] font-normal leading-[24px] text-left text-[#FAFAFA]' href="">
+                Privacy Policy
+              </a>
+            </li>
+            <li className='mt-[10px]'>
+              <a className='w-[94px] h-[24px] font-poppins text-[16px] font-normal leading-[24px] text-left text-[#FAFAFA]' href="">
+                Terms Of Use
+              </a>
+            </li>
+            <li className='mt-[10px]'>
+              <a className='w-[94px] h-[24px] font-poppins text-[16px] font-normal leading-[24px] text-left text-[#FAFAFA]' href="">
+                FAQ
+              </a>
+            </li>
+            <li className='mt-[10px]'>
+              <a className='w-[94px] h-[24px] font-poppins text-[16px] font-normal leading-[24px] text-left text-[#FAFAFA]' href="">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div className=''>
+          <ul>
+            <li className='mt-[20px]'>
+              <a className='w-[81px] h-[28px] font-poppins text-[20px] font-medium leading-[28px] text-left text-[#FAFAFA]' href="">
+                Download App
+              </a>
+            </li>
+            <li className='mt-[10px]'>
+              <a className='w-[94px] h-[24px] font-poppins text-[16px] font-normal leading-[24px] text-left text-[#FAFAFA]' href="">
+                Save $3 with App New User Only
+              </a>
+            </li>
+            <ul className='flex'>
+              <li>
+                <img className='ml-[20px] mt-[20px] w-[80px] h-[80px]' src={Rasm_36} alt="App Icon 1" />
+              </li>
+              <li>
+                <img className='ml-[20px] mt-[15px]' src={Rasm_37} alt="App Icon 2" />
+              </li>
+              <li>
+                <img className='ml-[20px] mt-[10px]' src={Rasm_38} alt="App Icon 3" />
+              </li>
+            </ul>
+            <ul className='flex gap-[30px] mt-[20px] ml-[10px]'>
+              <li>
+                <img className='' src={Rasm_39} alt="Social Icon 1" />
+              </li>
+              <li>
+                <img className='' src={Rasm_40} alt="Social Icon 2" />
+              </li>
+              <li>
+                <img className='' src={Rasm_41} alt="Social Icon 3" />
+              </li>
+              <li>
+                <img className='' src={Rasm_42} alt="Social Icon 4" />
+              </li>
+            </ul>
+          </ul>
         </div>  
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
+ 
