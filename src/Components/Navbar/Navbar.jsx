d5c96a7ac7c99c6264792b80c65219cd2c5eb0f1
Sarvarbek Yuldoshov <sarvarbekyuldoshov85@gmail.com>
2024-09-05 11:39:14 +0300

    salom

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,48 +1,72 @@
-import React from 'react'
-import Data from "../Data/Data"
-import useSharedStore from '../String/Store'
+import React from 'react';
+import Data from '../Data/Data';
+import useSharedStore from '../String/Store';
 
 const Navbar = () => {
-  const {setCards} = useSharedStore
+  // Correctly invoke useSharedStore
+  const { cards, setCards } = useSharedStore();
 
-  const handleAdd = (item) =>{
-    setCards([...cards,item])
-  }
+  // Function to add item to the cards state
+  const handleAdd = (item) => {
+    setCards([...cards, item]); // Spread the existing cards and add the new item
+  };
+
   return (
-    <div className=''>
+    <div>
       <div className='max-w-[1200px] mx-auto p-[20px]'>
         <div className='flex flex-wrap gap-[50px]'>
-          {
-            Data && Data.map((item, index) => (
-              <div key={index} className='w-[250px] h-[300px] p-4 bg-white shadow-md rounded-lg relative max-sm:w-[340px] group'>
+          {Data &&
+            Data.map((item, index) => (
+              <div
+                key={index}
+                className='w-[250px] h-[300px] p-4 bg-white shadow-md rounded-lg relative max-sm:w-[340px] group'
+              >
                 {/* Mahsulot rasmi */}
-                <img src={item.img1} alt={item.name} className='w-[140px] h-[150px] ml-[30px] max-sm:ml-[70px]' />
-                
+                <img
+                  src={item.img1}
+                  alt={item.name}
+                  className='w-[140px] h-[150px] ml-[30px] max-sm:ml-[70px]'
+                />
+
                 {/* Qo'shimcha rasmlar */}
-                <img src={item.img3} alt={item.name} className='w-[30px] h-[30px] absolute right-2 top-1' />
-                <img src={item.img4} alt={item.name} className='w-[30px] h-[30px] absolute right-2 top-10' />
-                
+                <img
+                  src={item.img3}
+                  alt={item.name}
+                  className='w-[30px] h-[30px] absolute right-2 top-1'
+                />
+                <img
+                  src={item.img4}
+                  alt={item.name}
+                  className='w-[30px] h-[30px] absolute right-2 top-10'
+                />
+
                 {/* Mahsulot nomi */}
                 <h1 className='text-xl font-bold mt-2 max-sm:ml-[40px]'>{item.name}</h1>
-                
+
                 {/* Narx va boshqa elementlar */}
                 <ul className='flex h-[40px] items-center gap-[5px] max-sm:ml-[20px]'>
                   <h2 className='text-lg text-gray-600'>{item.price}</h2>
-                  <img src={item.img2} alt={item.name} className='w-[140px] h-[20px]' />
+                  <img
+                    src={item.img2}
+                    alt={item.name}
+                    className='w-[140px] h-[20px]'
+                  />
                   <p className=''>{item.text}</p>
                 </ul>
-                
+
                 {/* Hover bo'lganda tugma ko'rinadi */}
-                <button onClick={()=>handleAdd(item)} className='mt-[20px] w-[250px] h-[60px] top-[220px]  bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 absolute bottom-4 left-1/2 transform -translate-x-1/2 opacity-0 group-hover:opacity-100 transition-opacity max-sm:w-[338px]'>
-                  Add 
+                <button
+                  onClick={() => handleAdd(item)}
+                  className='mt-[20px] w-[250px] h-[60px] top-[220px] bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 absolute bottom-4 left-1/2 transform -translate-x-1/2 opacity-0 group-hover:opacity-100 transition-opacity max-sm:w-[338px]'
+                >
+                  Add
                 </button>
               </div>
-            ))
-          }
+            ))}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Navbar;
--- src/Components/Shopping/Shopping.jsx
+++ src/Components/Shopping/Shopping.jsx
@@ -1,28 +1,49 @@
-import React, { useState } from 'react'
-import useSharedStore from '../String/Store'
+import React from 'react';
+import useSharedStore from '../String/Store';
 
 const Shopping = () => {
-  const {cards,setCards} = useSharedStore()
+  const { cards } = useSharedStore();
+
   return (
     <div className='max-w-[1200px] mx-auto p-[20px] justify-center'>
       <h1 className='ml-[400px] text-[30px]'>Sizning mahsulotlaringiz shu yerda</h1>
-      {
-        cards && cards.map((item,index)=>(
+       <div className='flex flex-wrap gap-[40px] mt-20'>
+      {cards.length > 0 ? (
+        cards.map((item, index) => (
           <div className='cards' key={index}>
-                <img src={item.img1} alt={item.name} className='w-[140px] h-[150px] ml-[30px] max-sm:ml-[70px]' />
-                <img src={item.img3} alt={item.name} className='w-[30px] h-[30px] absolute right-2 top-1' />
-                <img src={item.img4} alt={item.name} className='w-[30px] h-[30px] absolute right-2 top-10' />
-                <h1 className='text-xl font-bold mt-2 max-sm:ml-[40px]'>{item.name}</h1>
-                <ul className='flex h-[40px] items-center gap-[5px] max-sm:ml-[20px]'>
-                  <h2 className='text-lg text-gray-600'>{item.price}</h2>
-                  <img src={item.img2} alt={item.name} className='w-[140px] h-[20px]' />
-                  <p className=''>{item.text}</p>
-                </ul>
+            <img
+              src={item.img1}
+              alt={item.name}
+              className='w-[140px] h-[150px] ml-[30px] max-sm:ml-[70px]'
+            />
+            <img
+              src={item.img3}
+              alt={item.name}
+              className='w-[30px] h-[30px] absolute right-2 top-1'
+            />
+            <img
+              src={item.img4}
+              alt={item.name}
+              className='w-[30px] h-[30px] absolute right-2 top-10'
+            />
+            <h1 className='text-xl font-bold mt-2 max-sm:ml-[40px]'>{item.name}</h1>
+            <ul className='flex h-[40px] items-center gap-[5px] max-sm:ml-[20px]'>
+              <h2 className='text-lg text-gray-600'>{item.price}</h2>
+              <img
+                src={item.img2}
+                alt={item.name}
+                className='w-[140px] h-[20px]'
+              />
+              <p>{item.text}</p>
+            </ul>
           </div>
         ))
-      }
+      ) : (
+        <p>Hech qanday mahsulot yo'q</p>
+      )}
     </div>
-  )
-}
+   </div>
+  );
+};
 
-export default Shopping
+export default Shopping;
