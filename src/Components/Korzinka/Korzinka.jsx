2ff720096889567309efad1fee5c92a9952f151c
Sarvarbek Yuldoshov <sarvarbekyuldoshov85@gmail.com>
2024-09-08 14:07:09 +0300

    salom

--- src/Components/Korzinka/Korzinka.jsx
+++ src/Components/Korzinka/Korzinka.jsx
@@ -87,22 +87,18 @@ const Korzinka = () => {
 
     return (
         <div className=' max-w-[1200px] mx-auto p-4 justify-center'>
+
+            {/* ----------------------------------------map-qilingan qism------------------------------------------------------------ */}
+            
             {single.length > 0 ? (
                 single.map(item => (
                     <div key={item.id} className='flex flex-col items-center sm:flex-row sm:justify-center p-4'>
-                        <img src={item.img1} 
-                            alt={item.name} 
-                            className='w-[400px] h-[400px] sm:w-[300px] sm:h-[350px] mb-4 sm:mb-0' 
-                        />
-                        <div className='sm:ml-6 text-center sm:text-left'>
-                            <h1 className='text-[24px] font-bold mb-2'>{item.name}</h1>
-                            <div className='flex flex-col items-center sm:items-start'>
-                                <h2 className='text-[20px] text-gray-600 mb-2'>{item.price}</h2>
-                                <img 
-                                    src={item.img2} 
-                                    alt={item.name} 
-                                    className='w-[200px] h-[40px] mb-2' 
-                                />
+                        <img src={item.img1}  alt={item.name}className='w-[400px] h-[400px] sm:w-[300px] sm:h-[350px] mb-4 sm:mb-0'/>
+                    <div className='sm:ml-6 text-center sm:text-left'>
+                        <h1 className='text-[24px] font-bold mb-2'>{item.name}</h1>
+                    <div className='flex flex-col items-center sm:items-start'>
+                        <h2 className='text-[20px] text-gray-600 mb-2'>{item.price}</h2>
+                        <img src={item.img2} alt={item.name} className='w-[200px] h-[40px] mb-2'/>
                                 <p className='text-[16px] text-gray-500'>{item.text}</p>
                                 <p className='w-[400px]'>{item.text1}</p>
                             </div>
