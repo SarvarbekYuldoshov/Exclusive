e4edbcabb86cefc654339dcadb48398fd96e8631
Sarvarbek Yuldoshov <sarvarbekyuldoshov85@gmail.com>
2024-09-10 15:45:35 +0300

    salom

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,13 +1,15 @@
 import React, { useState } from 'react';
-import "./Hero.css";
-import Rasm_5 from "../../images/rasm5.png";
-import Rasm_6 from "../../images/rasm6.png";
-import Rasm_7 from "../../images/rasm7.png";
+import './Hero.css';
+import Rasm_7 from '../../images/rasm7.png';
+import Rasm_8 from '../../images/rasm43.png';
+import Rasm_9 from '../../images/rasm44.png';
 import { useTranslation } from 'react-i18next';
 import { Form, Input, Button, Modal, message } from 'antd';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/swiper-bundle.min.css';
-import { Autoplay, Pagination, Navigation } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+import { EffectCoverflow, Pagination } from 'swiper/modules';
 
 const Hero = () => {
   const { t } = useTranslation();
@@ -19,8 +21,8 @@ const Hero = () => {
       .then((values) => {
         const { name, surname, number } = values;
 
-        const token = "7288526920:AAH-vd_HYqMjr_qE5zG6idFBNxfFeMi9aFo";
-        const chat_id = 6801549705;
+        const token = 'YOUR_BOT_TOKEN';
+        const chat_id = 'YOUR_CHAT_ID';
         const url = `https://api.telegram.org/bot${token}/sendMessage`;
         const messageText = `Ism: ${name}\nFamiliya: ${surname}\nNumber: +998${number}`;
 
@@ -34,8 +36,8 @@ const Hero = () => {
             text: messageText,
           }),
         })
-          .then(res => res.json())
-          .then(res => {
+          .then((res) => res.json())
+          .then((res) => {
             if (res.ok) {
               message.success("Ma'lumot yuborildi");
               setOpen(false);
@@ -44,7 +46,7 @@ const Hero = () => {
               message.error("Yuborishda xatolik yuz berdi");
             }
           })
-          .catch(err => {
+          .catch((err) => {
             console.error(err);
             message.error("Xatolik yuz berdi, iltimos keyinroq urinib ko'ring");
           });
@@ -57,187 +59,81 @@ const Hero = () => {
   const showModal = () => setOpen(true);
   const closeModal = () => setOpen(false);
 
-  import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/effect-coverflow';
-import 'swiper/css/pagination';
-
-import './styles.css';
-
-// import required modules
-import { EffectCoverflow, Pagination } from 'swiper/modules';
-
   return (
-    <div className='bg-black text-center mt-[150px] max-sm:mt-[120px]'>
-      <div className='flex flex-col md:flex-row items-center mx-auto max-w-[1200px] p-6 md:p-10'>
+    <div className="bg-black text-center mt-[150px] max-sm:mt-[120px]">
+      <div className="flex flex-col md:flex-row items-center mx-auto max-w-[1200px] p-6 md:p-10">
         <Swiper
-          spaceBetween={30}
+          effect="coverflow"
+          grabCursor={true}
           centeredSlides={true}
-          autoplay={{
-            delay: 2500,
-            disableOnInteraction: false,
+          slidesPerView="auto"
+          coverflowEffect={{
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true,
           }}
-          pagination={{
-            clickable: true,
-          }}
-          navigation={true}
-          modules={[Autoplay, Pagination, Navigation]}
+          pagination={true}
+          modules={[EffectCoverflow, Pagination]}
           className="mySwiper"
         >
           <SwiperSlide>
-            <img className='' src={Rasm_5} alt="" />
-            <h1 className=''>{t("hero.hero_text_2")}</h1>
-            <Button className='mt-4 w-[150px] bg-blue-500 text-white hover:bg-blue-600' onClick={() => showModal({ name: 'Product Name', price: '100' })}>
-          {t("main.main_text_3")}
-          </Button>
-
-          <Modal open={open} footer={null} onCancel={closeModal}>
-            <h1 className='flex items-center justify-center text-[30px]'>Malumotlarni tuldiring</h1>
-            <p className=' justify-center text-[20px] text-[blue]'>Biz siz bilan imkon qadar tez bog`lanamiz va sizning mahsulotingzi yetkazib beramiz xizmatimiz bepul?</p>
-            <Form form={form} layout="vertical">
-              <Form.Item
-                label="Name"
-                name="name"
-                rules={[
-                  { required: true, message: 'Ismingizni kiriting' },
-                  { min: 5, message: 'Ism 5 tadan kam bo\'lmasligi kerak' }
-                ]}
-              >
-                <Input className='w-[480px] h-[50px]' placeholder='Ismingizni kiriting' />
-              </Form.Item>
-              <Form.Item
-               label="Surname"
-                name="surname"
-                rules={[
-                  { required: true, message: 'Familiyangizni kiriting' },
-                  { min: 5, message: 'Familiya 5 tadan kam bo\'lmasligi kerak' }
-                ]}
-              >
-                <Input  className='w-[480px] h-[50px]' placeholder='Familiyangizni kiriting' />
-              </Form.Item>
-              <Form.Item
-                label="Number"
-                name="number"
-                rules={[
-                  { required: true, message: 'Raqamingizni kiriting' },
-                  {
-                    pattern: /^\+998\d{9}$/,
-                    message: 'Telefon raqam +998 bilan boshlanib, 9 ta raqam bilan davom etishi kerak',
-                  },
-                ]}
-              >
-                <Input className='w-[480px] h-[50px]' placeholder='+998' />
-              </Form.Item>
-              <Button className='w-[480px] h-[50px]' onClick={sendMessage} type="primary">
-                Yuborish
-              </Button>
-            </Form>
-          </Modal>
+            <img src={Rasm_7} alt="Slide 1" />
           </SwiperSlide>
           <SwiperSlide>
-          <img className='' src={Rasm_5} alt="" />
-            <h1 className=''>{t("hero.hero_text_2")}</h1>
-            <Button className='mt-4 w-[150px] bg-blue-500 text-white hover:bg-blue-600' onClick={() => showModal({ name: 'Product Name', price: '100' })}>
-          {t("main.main_text_3")}
-          </Button>
-
-          <Modal open={open} footer={null} onCancel={closeModal}>
-            <h1 className='flex items-center justify-center text-[30px]'>Malumotlarni tuldiring</h1>
-            <p className=' justify-center text-[20px] text-[blue]'>Biz siz bilan imkon qadar tez bog`lanamiz va sizning mahsulotingzi yetkazib beramiz xizmatimiz bepul?</p>
-            <Form form={form} layout="vertical">
-              <Form.Item
-                label="Name"
-                name="name"
-                rules={[
-                  { required: true, message: 'Ismingizni kiriting' },
-                  { min: 5, message: 'Ism 5 tadan kam bo\'lmasligi kerak' }
-                ]}
-              >
-                <Input className='w-[480px] h-[50px]' placeholder='Ismingizni kiriting' />
-              </Form.Item>
-              <Form.Item
-               label="Surname"
-                name="surname"
-                rules={[
-                  { required: true, message: 'Familiyangizni kiriting' },
-                  { min: 5, message: 'Familiya 5 tadan kam bo\'lmasligi kerak' }
-                ]}
-              >
-                <Input  className='w-[480px] h-[50px]' placeholder='Familiyangizni kiriting' />
-              </Form.Item>
-              <Form.Item
-                label="Number"
-                name="number"
-                rules={[
-                  { required: true, message: 'Raqamingizni kiriting' },
-                  {
-                    pattern: /^\+998\d{9}$/,
-                    message: 'Telefon raqam +998 bilan boshlanib, 9 ta raqam bilan davom etishi kerak',
-                  },
-                ]}
-              >
-                <Input className='w-[480px] h-[50px]' placeholder='+998' />
-              </Form.Item>
-              <Button className='w-[480px] h-[50px]' onClick={sendMessage} type="primary">
-                Yuborish
-              </Button>
-            </Form>
-          </Modal>
+            <img src={Rasm_8} alt="Slide 2" />
           </SwiperSlide>
           <SwiperSlide>
-          <img className='' src={Rasm_5} alt="" />
-            <h1 className=''>{t("hero.hero_text_2")}</h1>
-            <Button className='mt-4 w-[150px] bg-blue-500 text-white hover:bg-blue-600' onClick={() => showModal({ name: 'Product Name', price: '100' })}>
-          {t("main.main_text_3")}
-          </Button>
-
-          <Modal open={open} footer={null} onCancel={closeModal}>
-            <h1 className='flex items-center justify-center text-[30px]'>Malumotlarni tuldiring</h1>
-            <p className=' justify-center text-[20px] text-[blue]'>Biz siz bilan imkon qadar tez bog`lanamiz va sizning mahsulotingzi yetkazib beramiz xizmatimiz bepul?</p>
-            <Form form={form} layout="vertical">
-              <Form.Item
-                label="Name"
-                name="name"
-                rules={[
-                  { required: true, message: 'Ismingizni kiriting' },
-                  { min: 5, message: 'Ism 5 tadan kam bo\'lmasligi kerak' }
-                ]}
-              >
-                <Input className='w-[480px] h-[50px]' placeholder='Ismingizni kiriting' />
-              </Form.Item>
-              <Form.Item
-               label="Surname"
-                name="surname"
-                rules={[
-                  { required: true, message: 'Familiyangizni kiriting' },
-                  { min: 5, message: 'Familiya 5 tadan kam bo\'lmasligi kerak' }
-                ]}
-              >
-                <Input  className='w-[480px] h-[50px]' placeholder='Familiyangizni kiriting' />
-              </Form.Item>
-              <Form.Item
-                label="Number"
-                name="number"
-                rules={[
-                  { required: true, message: 'Raqamingizni kiriting' },
-                  {
-                    pattern: /^\+998\d{9}$/,
-                    message: 'Telefon raqam +998 bilan boshlanib, 9 ta raqam bilan davom etishi kerak',
-                  },
-                ]}
-              >
-                <Input className='w-[480px] h-[50px]' placeholder='+998' />
-              </Form.Item>
-              <Button className='w-[480px] h-[50px]' onClick={sendMessage} type="primary">
-                Yuborish
-              </Button>
-            </Form>
-          </Modal>
+            <img src={Rasm_9} alt="Slide 3" />
           </SwiperSlide>
         </Swiper>
       </div>
+
+      <Modal open={open} footer={null} onCancel={closeModal}>
+        <h1 className="text-center text-[30px]">Ma'lumotlarni to'ldiring</h1>
+        <p className="text-center text-[20px] text-[blue]">
+          Biz siz bilan imkon qadar tez bog'lanamiz va sizning mahsulotingizni yetkazib beramiz. Xizmatimiz bepul!
+        </p>
+        <Form form={form} layout="vertical">
+          <Form.Item
+            label="Ism"
+            name="name"
+            rules={[
+              { required: true, message: 'Ismingizni kiriting' },
+              { min: 5, message: 'Ism 5 tadan kam bo\'lmasligi kerak' },
+            ]}
+          >
+            <Input className="w-[480px] h-[50px]" placeholder="Ismingizni kiriting" />
+          </Form.Item>
+          <Form.Item
+            label="Familiya"
+            name="surname"
+            rules={[
+              { required: true, message: 'Familiyangizni kiriting' },
+              { min: 5, message: 'Familiya 5 tadan kam bo\'lmasligi kerak' },
+            ]}
+          >
+            <Input className="w-[480px] h-[50px]" placeholder="Familiyangizni kiriting" />
+          </Form.Item>
+          <Form.Item
+            label="Telefon raqam"
+            name="number"
+            rules={[
+              { required: true, message: 'Raqamingizni kiriting' },
+              {
+                pattern: /^\d{9}$/,
+                message: 'Telefon raqam 9 ta raqamdan iborat bo\'lishi kerak',
+              },
+            ]}
+          >
+            <Input className="w-[480px] h-[50px]" placeholder="Telefon raqamingizni kiriting" />
+          </Form.Item>
+          <Button className="w-[480px] h-[50px]" onClick={sendMessage} type="primary">
+            Yuborish
+          </Button>
+        </Form>
+      </Modal>
     </div>
   );
 };
