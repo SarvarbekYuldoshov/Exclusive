ffa4dd3c0ab8bab8d38ce56489be7eb44ece32af
Sarvarbek Yuldoshov <sarvarbekyuldoshov85@gmail.com>
2024-09-17 13:16:46 +0300

    salom

--- src/Components/Data/Data.jsx
+++ src/Components/Data/Data.jsx
@@ -1,16 +1,17 @@
-import Rasm_1 from "../../images/rasm16.png"
-import Rasm_16 from "../../images/col1.png"
-import Rasm_3 from "../../images/rasm21.png"
-import Rasm_4 from "../../images/rasm45.png"
-import Rasm_7 from "../../images/rasm25.png"
-import Rasm_8 from "../../images/rasm19.png"
-import Rasm_9 from "../../images/rasm17.png"
-import Rasm10 from "../../images/img.png"
-import Rasm11 from "../../images/img1.png"
-import Rasm12 from "../../images/img2.png"  
-import Rasm13 from "../../images/img3.png"
-import Rasm14 from "../../images/img4.png"
-import Rasm15 from "../../images/img5.png"
+import Rasm_1 from "../../images/rasm16.png";
+import Rasm_16 from "../../images/col1.png";
+import Rasm_3 from "../../images/rasm21.png";
+import Rasm_4 from "../../images/rasm45.png";
+import Rasm_7 from "../../images/rasm25.png";
+import Rasm_8 from "../../images/rasm19.png";
+import Rasm_9 from "../../images/rasm17.png";
+import Rasm10 from "../../images/img.png";
+import Rasm11 from "../../images/img1.png";
+import Rasm12 from "../../images/img2.png";  
+import Rasm13 from "../../images/img3.png";
+import Rasm14 from "../../images/img4.png";
+import Rasm15 from "../../images/img5.png";
+
 const Data = [
     {
         id: 1,
@@ -22,200 +23,24 @@ const Data = [
         img8: Rasm14,
         img9: Rasm15,
         img10: Rasm_4,
-        img11:Rasm_16,
-        name: "Breed Dry Dog Food",
+        img11: Rasm_16,
+        nameen: "Breed Dry Dog Food",
+        nameuz: "Quruq itlar uchun yem",
         price: "$100",
-        img2:Rasm_8,
-        text:"(35)",
-        text1uz:"Boxed meals, also known as pre-packaged meals, are convenient and ready-to-eat food options. They often include a variety of items such as sandwiches, salads, or snacks. Ideal for on-the-go consumption, boxed meals are popular for lunches, picnics, or travel. They offer a quick, easy, and mess-free dining solution.",
-        link:"@Sirojidinovich_TDIU",
-        text2:"Agarda Qushimcha savolar bulsa shu joyga bog`laning",
-        text3:"M-Razmerda",
-        text4:"XL-Razmerda",
-        text5:"2LX-Razmerda",
-        text6:"Free Delivery",
-        text7:"Enter your postal code for Delivery Availability",
-        text8:"Return Delivery",
-        text9:"Free 30 Days Delivery Returns. Details"
-        
+        img2: Rasm_8,
+        text: "(35)",
+        text1en: "Boxed meals, also known as pre-packaged meals, are convenient and ready-to-eat food options. They often include a variety of items such as sandwiches, salads, or snacks. Ideal for on-the-go consumption, boxed meals are popular for lunches, picnics, or travel. They offer a quick, easy, and mess-free dining solution.",
+        text1uz: "Oldindan qadoqlangan taomlar sifatida ham tanilgan qutili ovqatlar qulay va tayyor ovqat variantidir. Ular ko'pincha sendvichlar, salatlar yoki gazaklar kabi turli xil narsalarni o'z ichiga oladi. Yo'l-yo'lakay iste'mol qilish uchun ideal, qutidagi ovqatlar tushlik, piknik yoki sayohat uchun mashhur. Ular tez, oson va tartibsiz ovqatlanish yechimini taklif qilishadi.",
+        link: "@Sirojidinovich_TDIU",
+        text6en: "Free Delivery",
+        text6uz: "Bepul yetkazib berish",
+        text7en: "Enter your postal code for Delivery Availability",
+        text7uz: "Etkazib berish mavjudligi uchun pochta indeksingizni kiriting",
+        text8en: "Return Delivery",
+        text8uz: "Qaytish yetkazib berish",
+        text9en: "Free 30 Days Delivery Returns. Details",
+        text9uz: "Bepul 30 kun yetkazib berish qaytariladi. Tafsilotlar"
     },
-    {
-        id: 2,
-        img3: Rasm_9,
-        img4: Rasm10,
-        img5: Rasm11,
-        img6: Rasm12,
-        img7: Rasm13,
-        img8: Rasm14,
-        img9: Rasm15,
-        img1: Rasm_4,
-        img11:Rasm_16,
-        name: "CANON EOS DSLR Camera",
-        price: "$120",
-        img2:Rasm_8,
-        text:"(35)",
-        text1:"Boxed meals, also known as pre-packaged meals, are convenient and ready-to-eat food options. They often include a variety of items such as sandwiches, salads, or snacks. Ideal for on-the-go consumption, boxed meals are popular for lunches, picnics, or travel. They offer a quick, easy, and mess-free dining solution."
-        ,link:"@Sirojidinovich_TDIU",
-        text2:"Agarda Qushimcha savolar bulsa shu joyga boglaning" ,
-        text3:"M-Razmerda",
-        text4:"XL-Razmerda",
-        text5:"2LX-Razmerda",
-        text6:"Free Delivery",
-        text7:"Enter your postal code for Delivery Availability",
-        text8:"Return Delivery",
-        text9:"Free 30 Days Delivery Returns. Details"
-    },
-    {
-        id: 3,
-        img1: Rasm_3,
-        img3: Rasm_9,
-        img4: Rasm10,
-        img5: Rasm11,
-        img6: Rasm12,
-        img7: Rasm13,
-        img8: Rasm14,
-        img9: Rasm15,
-        img11:Rasm_16,
-        name: "ASUS FHD Gaming Laptop",
-        price: "$230",
-        img2:Rasm_8,
-        text:"(35)",
-        text1:"Boxed meals, also known as pre-packaged meals, are convenient and ready-to-eat food options. They often include a variety of items such as sandwiches, salads, or snacks. Ideal for on-the-go consumption, boxed meals are popular for lunches, picnics, or travel. They offer a quick, easy, and mess-free dining solution."
-        ,link:"@Sirojidinovich_TDIU",
-        text2:"Agarda Qushimcha savolar bulsa shu joyga boglaning" ,
-        text3:"M-Razmerda",
-        text4:"XL-Razmerda",
-        text5:"2LX-Razmerda",
-        text6:"Free Delivery",
-        text7:"Enter your postal code for Delivery Availability",
-        text8:"Return Delivery",
-        text9:"Free 30 Days Delivery Returns. Details"
-    },
-    {
-        id: 4,
-        img3: Rasm_9,
-        img4: Rasm10,
-        img5: Rasm11,
-        img6: Rasm12,
-        img7: Rasm13,
-        img1: Rasm14,
-        img9: Rasm15,
-        img11:Rasm_16,
-        name: "Curology Product Set ",
-        price: "$340",
-        img2:Rasm_8,
-        text:"(35)",
-        text1:"Boxed meals, also known as pre-packaged meals, are convenient and ready-to-eat food options. They often include a variety of items such as sandwiches, salads, or snacks. Ideal for on-the-go consumption, boxed meals are popular for lunches, picnics, or travel. They offer a quick, easy, and mess-free dining solution."
-        ,link:"@Sirojidinovich_TDIU",
-        text2:"Agarda Qushimcha savolar bulsa shu joyga boglaning" ,
-        text3:"M-Razmerda",
-        text4:"XL-Razmerda",
-        text5:"2LX-Razmerda",
-        text6:"Free Delivery",
-        text7:"Enter your postal code for Delivery Availability",
-        text8:"Return Delivery",
-        text9:"Free 30 Days Delivery Returns. Details"
-    },
-    {
-        id: 5,
-        img3: Rasm_9,
-        img4: Rasm10,
-        img5: Rasm11,
-        img1: Rasm12,
-        img8: Rasm14,
-        img9: Rasm15,
-        img11:Rasm_16,
-        name: "Kids Electric Car",
-        price: "$210",
-        img2:Rasm_8,
-        text:"(35)",
-        text1:"Boxed meals, also known as pre-packaged meals, are convenient and ready-to-eat food options. They often include a variety of items such as sandwiches, salads, or snacks. Ideal for on-the-go consumption, boxed meals are popular for lunches, picnics, or travel. They offer a quick, easy, and mess-free dining solution."
-        ,link:"@Sirojidinovich_TDIU",
-        text2:"Agarda Qushimcha savolar bulsa shu joyga boglaning" ,
-        text3:"M-Razmerda",
-        text4:"XL-Razmerda",
-        text5:"2LX-Razmerda",
-        text6:"Free Delivery",
-        text7:"Enter your postal code for Delivery Availability",
-        text8:"Return Delivery",
-        text9:"Free 30 Days Delivery Returns. Details"
-    },
-    {
-        id: 6,
-        img3: Rasm_9,
-        img4: Rasm10,
-        img5: Rasm11,
-        img6: Rasm12,
-        img7: Rasm13,
-        img8: Rasm14,
-        img1: Rasm15,
-        img11:Rasm_16,
-        name: "Jr. Zoom Soccer Cleats",
-        price: "$960",
-        img2:Rasm_8,
-        text:"(35)",
-        text1:"Boxed meals, also known as pre-packaged meals, are convenient and ready-to-eat food options. They often include a variety of items such as sandwiches, salads, or snacks. Ideal for on-the-go consumption, boxed meals are popular for lunches, picnics, or travel. They offer a quick, easy, and mess-free dining solution."
-        ,link:"@Sirojidinovich_TDIU",
-        text2:"Agarda Qushimcha savolar bulsa shu joyga boglaning" ,
-        text3:"M-Razmerda",
-        text4:"XL-Razmerda",
-        text5:"2LX-Razmerda",
-        text6:"Free Delivery",
-        text7:"Enter your postal code for Delivery Availability",
-        text8:"Return Delivery",
-        text9:"Free 30 Days Delivery Returns. Details"
-    },
-    {
-        id: 7,
-        img1: Rasm_7,
-        img3: Rasm_9,
-        img4: Rasm10,
-        img5: Rasm11,
-        img1: Rasm12,
-        img7: Rasm13,
-        img8: Rasm14,
-        img9: Rasm15,
-        img11:Rasm_16,
-        name: "GP11 Shooter USB Gamepad",
-        price: "$1100",
-        img2:Rasm_8,
-        text:"(35)",
-        text1:"Boxed meals, also known as pre-packaged meals, are convenient and ready-to-eat food options. They often include a variety of items such as sandwiches, salads, or snacks. Ideal for on-the-go consumption, boxed meals are popular for lunches, picnics, or travel. They offer a quick, easy, and mess-free dining solution."
-        ,link:"@Sirojidinovich_TDIU",
-        text2:"Agarda Qushimcha savolar bulsa shu joyga boglaning" ,
-        text3:"M-Razmerda",
-        text4:"XL-Razmerda",
-        text5:"2LX-Razmerda",
-        text6:"Free Delivery",
-        text7:"Enter your postal code for Delivery Availability",
-        text8:"Return Delivery",
-        text9:"Free 30 Days Delivery Returns. Details"
-    },
-    {
-        id: 8,
-        img1: Rasm_1,
-        img3: Rasm_9,
-        img4: Rasm10,
-        img5: Rasm11,
-        img6: Rasm12,
-        img7: Rasm13,
-        img8: Rasm14,
-        img1: Rasm_3,
-        img11:Rasm_16,
-        name: "Quilted Satin Jacket",
-        price: "$140",
-        img2:Rasm_8,
-        text:"(35)",
-        text1:"Boxed meals, also known as pre-packaged meals, are convenient and ready-to-eat food options. They often include a variety of items such as sandwiches, salads, or snacks. Ideal for on-the-go consumption, boxed meals are popular for lunches, picnics, or travel. They offer a quick, easy, and mess-free dining solution."
-        ,link:"@Sirojidinovich_TDIU",
-        text2:"Agarda Qushimcha savolar bulsa shu joyga boglaning" ,
-        text3:"M-Razmerda",
-        text4:"XL-Razmerda",
-        text5:"2LX-Razmerda",
-        text6:"Free Delivery",
-        text7:"Enter your postal code for Delivery Availability",
-        text8:"Return Delivery",
-        text9:"Free 30 Days Delivery Returns. Details"
-    },
-]
+];
+
 export default Data;
