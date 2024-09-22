c3b7c268639c7f432de9ae7bfa11c0ee00a63430
Sarvarbek Yuldoshov <sarvarbekyuldoshov85@gmail.com>
2024-09-22 23:11:11 +0300

    salom

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -1,10 +1,5 @@
 import React, { useState } from 'react';
 import "./Menu.css";
-import Rasm_27 from "../../images/rasm27.png";
-import Rasm_28 from "../../images/rasm28.png";
-import Rasm_29 from "../../images/rasm29.png";
-import Rasm_30 from "../../images/rasm30.png";
-import Rasm_31 from "../../images/rasm31.png";
 import { useTranslation } from 'react-i18next';
 import { Button, Modal, message, Input, Form } from 'antd';
 
@@ -24,7 +19,7 @@ const CustomModal = ({ visible, onClose, form, sendMessage }) => {
             { min: 5, message: "Ism 5 ta belgidan kam bo'lmasligi kerak" }
           ]}
         >
-          <Input className='w-[480px] h-[50px]' placeholder='Ismingizni kiriting' />
+          <Input className='w-full h-[50px]' placeholder='Ismingizni kiriting' />
         </Form.Item>
         <Form.Item
           label="Surname"
@@ -34,7 +29,7 @@ const CustomModal = ({ visible, onClose, form, sendMessage }) => {
             { min: 5, message: "Familiya 5 ta belgidan kam bo'lmasligi kerak" }
           ]}
         >
-          <Input className='w-[480px] h-[50px]' placeholder='Familiyangizni kiriting' />
+          <Input className='w-full h-[50px]' placeholder='Familiyangizni kiriting' />
         </Form.Item>
         <Form.Item
           label="Number"
@@ -47,9 +42,9 @@ const CustomModal = ({ visible, onClose, form, sendMessage }) => {
             }
           ]}
         >
-          <Input className='w-[480px] h-[50px]' placeholder='+998' />
+          <Input className='w-full h-[50px]' placeholder='+998' />
         </Form.Item>
-        <Button className='w-[480px] h-[50px]' onClick={sendMessage} type="primary">
+        <Button className='w-full h-[50px] mt-4' onClick={sendMessage} type="primary">
           Yuborish
         </Button>
       </Form>
@@ -69,7 +64,7 @@ const Menu = () => {
         const token = "7288526920:AAH-vd_HYqMjr_qE5zG6idFBNxfFeMi9aFo";
         const chat_id = "6801549705";
         const url = `https://api.telegram.org/bot${token}/sendMessage`;
-        const messageText = `Ism: ${name}\nFamiliya: ${surname}\nNumber: ${number}\nMahsulot: ${selectedItem?.name}\nNarxi: ${selectedItem?.price}`;
+        const messageText = `Ism: ${name}\nFamiliya: ${surname}\nNumber: ${number}`;
 
         fetch(url, {
           method: 'POST',
@@ -99,77 +94,14 @@ const Menu = () => {
   };
 
   return (
-    <div id='navbar' className=''>
+    <div id='navbar'>
       <div className='mx-auto max-w-[1200px] p-[20px]'>
-        <ul className=''>
-          <li className='flex items-center gap-[10px]'>
-            <img className='w-[20px] h-[40px]' src={Rasm_27} alt="" />
-            <p className='w-[130px] h-[20px] font-poppins text-[16px] font-semibold leading-[20px] text-left text-[#DB4444]'>
-              {t("menu.menu_text_1")}
-            </p>
-          </li>
-          <h1 className='mt-[20px] w-[306px] h-[48px] font-inter text-[36px] font-semibold leading-[48px] tracking-[0.04em] text-left text-black'>
-            {t("menu.menu_text_2")}
-          </h1>
+        <ul>
+          {/* Other content */}
         </ul>
 
         <div className='mt-[60px] flex flex-wrap justify-between'>
-          {/* Card 1 */}
-          <div className='w-[100%] lg:w-[48%] bg-black relative p-[20px] mb-[20px]'>
-            <h1 className='text-[24px] font-semibold text-left text-[#FAFAFA]'>
-              {t("menu.menu_text_3")}
-            </h1>
-            <p className='mt-[20px] text-[14px] font-normal text-left text-[#FAFAFA]'>
-              {t("menu.menu_text_4")}
-            </p>
-            <Button className='mt-4 w-[150px] bg-[black] text-white hover:bg-blue-600' onClick={showModal}>
-              {t("main.main_text_3")}
-            </Button>
-            <img className='w-[280px] bg-cover' src={Rasm_28} alt="" />
-          </div>
-
-          {/* Card 2 */}
-          <div className='w-[100%] lg:w-[48%] bg-black p-[20px] mb-[20px] relative'>
-            <h2 className='text-[24px] font-semibold text-left text-[#FAFAFA]'>
-              {t("menu.menu_text_6")}
-            </h2>
-            <p className='mt-[20px] text-[14px] font-normal text-left text-[#FAFAFA]'>
-              {t("menu.menu_text_7")}
-            </p>
-            <Button className='mt-4 w-[150px] bg-[black] text-white hover:bg-blue-600' onClick={showModal}>
-              {t("main.main_text_3")}
-            </Button>
-            <img className='w-[180px]' src={Rasm_29} alt="" />
-          </div>
-
-          {/* Card 3 */}
-          <div className='w-[100%] lg:w-[48%] bg-black p-[20px] mb-[20px] relative'>
-            <img className='w-[150px] h-[150px] absolute top-[-20px] right-0' src={Rasm_30} alt="" />
-            <h3 className='mt-[120px] text-[24px] font-semibold text-left text-[#FAFAFA]'>
-              {t("menu.menu_text_9")}
-            </h3>
-            <p className='mt-[20px] text-[14px] font-normal text-left text-[#FAFAFA]'>
-              {t("menu.menu_text_11")}
-            </p>
-            <Button className='mt-4 w-[150px] bg-[black] text-white hover:bg-blue-600' onClick={showModal}>
-              {t("main.main_text_3")}
-            </Button>
-          </div>
-
-          {/* Card 4 */}
-          <div className='w-[100%] lg:w-[48%] bg-black p-[20px] mb-[20px] relative'>
-            <img className='w-[180px] h-[180px] absolute top-[-20px] right-0' src={Rasm_31} alt="" />
-            <h4 className='mt-[120px] text-[24px] font-semibold text-left text-[#FAFAFA]'>
-             
-            {t("menu.menu_text_13")}
-            </h4>
-            <p className='mt-[20px] text-[15px] font-normal text-left text-[#FAFAFA]'>
-              {t("menu.menu_text_14")}
-            </p>
-            <Button className='mt-4 w-[150px] bg-[black] text-white hover:bg-blue-600' onClick={showModal}>
-              {t("main.main_text_3")}
-            </Button>
-          </div>
+          {/* Card components */}
         </div>
 
         <CustomModal visible={open} onClose={closeModal} form={form} sendMessage={sendMessage} />
@@ -179,3 +111,4 @@ const Menu = () => {
 };
 
 export default Menu;
+ 
